9bb67187370f7680513c0d129ba18a7b6899af1f
Michael Juszkiewicz <mikejuszkie@gmail.com>
2016-03-16 22:38:01 +0000

    fixed formating and compiler warnigs

--- loans.c
+++ loans.c
@@ -18,7 +18,9 @@
 // initialize gloabal variables
 
 
-float Calculate_Interest(struct loan account, uint8_t account_number ,uint16_t number_of_days)
+float Calculate_Interest(struct loan* account,
+			 uint8_t account_number,
+			 uint16_t number_of_days)
 {
 //  float total = (account[account_number].principle *
 //		 (account[account_number].rate / 365) ) * number_of_days;
@@ -27,9 +29,14 @@ float Calculate_Interest(struct loan account, uint8_t account_number ,uint16_t n
 }
 
 
-uint16_t Calculate_Payments(struct loan account, uint8_t account_number, float payment)
+uint16_t Calculate_Payments(struct loan* account,
+			    uint8_t account_number,
+			    float payment)
 {
+  
   int number_of_payments = 0;
+
+  /*
   float principle = account[account_number].principle;
   float rate = account[account_number].rate;
   float interest = account[account_number].interest;
@@ -42,7 +49,8 @@ uint16_t Calculate_Payments(struct loan account, uint8_t account_number, float p
       number_of_payments++;
     }
 
-
+  */
   
   return number_of_payments;
 }
+
--- main.c
+++ main.c
@@ -22,6 +22,8 @@ float g_account_total = 0.00;
 int g_number_of_accounts = 0;
 float g_monthly_payment = 0.00;
 
+int length;
+
 struct loan account [MAX_NUMBER_OF_ACCOUNTS];
 
 int main()
@@ -29,10 +31,11 @@ int main()
   for(int i =0 ; i <= MAX_NUMBER_OF_ACCOUNTS - 1; i++)
     {
       printf("Eneter a name for account %d (enter \"done\" to finish):\n", i);
-      scanf("%s", &account[i].name);
-      
+      scanf("%s", account[i].name);
+            
       if ( *account[i].name == *"done" )
-          break;
+	  break;
+
       
       printf("Eneter inital loan ammount for account %d :\n", i);
       scanf("%f", &account[i].ammount);
@@ -42,17 +45,18 @@ int main()
       g_number_of_accounts++;
       
     }
-  printf("\n\n\n\tAccount Name\t\tInital Value\t\tInterest Rate\n");
+  printf("\n\n\n\t%-20s|%-20s|%-20s\n", "Account Name",
+	 "Inital Ammount","Interest Rate" );
   printf("\t---------------------------------------------------------------\n");
   
   for(int i = 0 ; i <= g_number_of_accounts - 1; i++)
     {
-      printf("\t%10s\t\t$%10.2f\t\t%10.2f\%\n", account[i].name,
+      printf("\t%-20s|$%19.2f|%20.2f%%\n", account[i].name,
                 account[i].ammount, account[i].rate);
     }
 
   printf("\nPlease enter your monthly controbution :");
-  scanf("%f", g_monthly_payment);
+  scanf("%f", &g_monthly_payment);
   
 //  uint16_t total_payments = Calculate_Payments(0, g_monthly_payment);
   
