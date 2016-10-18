57647b94684ebba9ddcf2d698585398653e2afd1
Michael Juszkiewicz ; 43245 <mjuszkie@hw-rh66-1.mc.com>
2016-10-18 11:58:22 -0400

    updated Makefile to operate in c99 mode. and fixes

--- main.c
+++ main.c
@@ -37,20 +37,20 @@ int main()
 // TODO: BUG
 // Print out of large numbers result in incorrect printout.
 // Subition of a string for float variables results in multiple submissions.
-      
+
       printf("Enter inital loan ammount for account %d :\n", i);
       scanf("%f", &account[i].ammount);
       account[i].principle = account[i].ammount;
-      
+
       printf("Enter interest rate for account %d :\n", i);
       scanf("%f", &account[i].rate);
       g_number_of_accounts++;
-      
+
     }
   printf("\n\n\n\t%-20s| %-15s| %-12s\n", "Account Name",
 	 "Inital Ammount","Interest Rate" );
   printf("\t-----------------------------------------------------\n");
-  
+
   for(int i = 0 ; i < g_number_of_accounts ; i++)
     {
       printf("\t%-20s| $%13.2f | %7.2f%%\n", account[i].name,
@@ -65,12 +65,12 @@ int main()
       while(account[i].monthly_payment <= 1.25*(30*((account[i].ammount*(account[i].rate/100))/365)))
 	{
 	  printf("Payment amount is insuficient. Please try again.\n");
-	  scanf("%f", &account[i].monthly_payment); 
-	  
+	  scanf("%f", &account[i].monthly_payment);
+
 	}
 
       Calculate_Payments(account , i , account[i].monthly_payment);
-      
+
       account[i].total_paid = ( (float)account[i].monthly_payment * account[i].payments ) + account[i].principle;
 
       // uncomment Line below for debug. 
@@ -85,14 +85,14 @@ int main()
 
   for(int i = 0 ; i < g_number_of_accounts ; i++)
     {
-      
+
       printf("%-20s| $%12.2f |%11.2f%%   | $%15.2f |%12d        | $%14.2f\n",
 	     account[i].name,account[i].ammount, account[i].rate,
 	     account[i].monthly_payment,account[i].payments,
 	     account[i].total_paid );
-    
+
     }
 
-  
+
   return 0;
 }
